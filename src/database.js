dfbcd223e783988ef899dade237fd71f23427736
stephensander1161 <57675208+stephensander1161@users.noreply.github.com>
2021-02-25 15:45:11 -0700

    Delete database.js

--- src/database.js
+++
@@ -1,9 +0,0 @@
-import {MongoClient} from 'mongodb';
-
-const url = 'mongodb://localhost:27017/fileapp';
-
-export const connect = (callback) => {
-    MongoClient.connect(url, (err, db) => {
-        return callback(err, db);
-});
-};
